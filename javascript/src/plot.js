199ea026ecf999ce0323a681f1ce164c4040ed7f
mennovf <mennovanfrachem@hotmail.com>
2015-07-29 17:54:11 +0200

    magnitude plot works

--- javascript/src/plot.js
+++ javascript/src/plot.js
@@ -1,4 +1,4 @@
-var dygraphs = require('dygraphs');
+var Dygraph = require('dygraphs');
 var system = require('./system');
 var _ = require('underscore');
 var math = require('mathjs');
@@ -13,19 +13,37 @@ module.exports = {
      * @returns {Dygraph} - A reference to the plot.
      */
     bode: function(container, system) {
-        var omegas = num.linspace(math.pow(10, -1), math.pow(10, 3), 1000);
-        var data = _.zip(omegas, _.map(omegas, function(omega) { return system.eval(math.complex(0, omega)); }));
-        var options = {
-            labels: ['omega (rad/s)', '|H(omega) (dB)|'],
+        function div_half_height() {
+            var d = document.createElement('div');
+            d.style.width = container.style.width;
+            d.style.height = container.style.offsetHeight / 2 + "px";
+            container.appendChild(d);
+            return d;
+        }
+
+        var magnitude_div = div_half_height(container);
+        var phase_div = div_half_height(container);
+
+        var omegas = num.logspace(-1, 3, 1000);
+        var magnitudes_data = _.zip(omegas, _.map(omegas, function(omega) 
+                                       {
+                                           return math.multiply(20, math.log10(math.abs(system.eval(math.complex(0, omega))))); 
+                                       }));
+        var common_options = {
             axes: {
                 x: {
-                    logscale: true
-                },
-                y: {
                     logscale: true
                 }
             }
         };
-        return new Dygraph(container, data, options);
+
+        var magnitude_options = {
+            title: 'Magnitude',
+            labels: ['omega', 'magnitude'],
+            xlabel: 'omega (rad/s)',
+            ylabel: '|H(omega) (dB)|',
+        };
+
+        return new Dygraph(magnitude_div, magnitudes_data, _.extend(common_options, magnitude_options));
     }
 };
--- javascript/test/bode/main.js
+++ javascript/test/bode/main.js
@@ -1,12 +1,7 @@
-require.config({
-    baseUrl: '../../src',
-    paths : {
-        plot: "plot",
-        system: "system"
-    }
-})
-require(['plot', 'system'], function(plots, system) {
-    var container = document.getElementById('bode-plot');
-    var sys = system.zpk([1], [10], 1);
-    var plot = plots.bode(container, sys);
-});
+var control = require('../../control.js'),
+    system = control.system,
+    plot = control.plot;
+
+var container = document.getElementById('bode-plot');
+var sys = system.zpk([1], [10], 1);
+var plt = plot.bode(container, sys);
