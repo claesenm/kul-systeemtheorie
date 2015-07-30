275d810cda55d58bceffc32248e6d5668d5c09ef
mennovf <mennovanfrachem@hotmail.com>
2015-07-30 16:40:46 +0200

    Add rough pzmap, needs improvement

--- javascript/src/plot.js
+++ javascript/src/plot.js
@@ -1,4 +1,6 @@
 var Dygraph = require('dygraphs');
+var synchronize = require('../lib/dygraph-extras/synchronizer');
+var shapes = require('../lib/dygraph-extras/shapes');
 var _ = require('underscore');
 var math = require('mathjs');
 var system = require('./system');
@@ -14,9 +16,10 @@ module.exports = {
      * Plots a bode plot of the given system in container.
      * @param {HTMLElement} container - The container in which to show the plot.
      * @param {System} system - The system of which to show the bode plot.
+     * @param {Array<Number>} omega_bounds - The boundaries of the pulsation to plot in logspace (e.g. 10^2 is entered as 2).
      * @returns {Array<Dygraph>} - An array of 2 plots. The first is the magnitude plot and the second is the phase plot.
      */
-    bode: function(container, system) {
+    bode: function(container, system, omega_bounds) {
         function div_half_height() {
             var d = document.createElement('div');
             d.style.width = container.offsetWidth + "px";
@@ -28,7 +31,7 @@ module.exports = {
         var magnitude_div = div_half_height(container);
         var phase_div = div_half_height(container);
 
-        omega_bounds = this.interesting_region_logspace(system);
+        omega_bounds = omega_bounds || this.interesting_region_logspace(system);
         var omegas = num.logspace(omega_bounds[0], omega_bounds[1], 1000);
 
 
@@ -36,10 +39,10 @@ module.exports = {
         var magnitudes_data = _.zip(omegas, _.map(evaluated_omegas, function(H) { return 20 * math.log10(math.abs(H)); }));
         var phases_data = _.zip(omegas, _.map(evaluated_omegas, function(omega) { return 180 / math.pi * math.arg(omega); }));
 
-        var common_options = {
+        var options = {
             legend: 'follow',
             labelsDivStyles: {
-                backgroundColor:  "rgba(0, 0, 0, 0)"
+                'background-color':  'rgba(0, 0, 0, 0)'
             },
             axes: {
                 x: {
@@ -53,7 +56,7 @@ module.exports = {
 
         var magnitude_options = {
             labels: ['Pulsation', 'Magnitude'],
-            xlabel: 'Pulsation (rad/s)',
+            xlabel: '',
             ylabel: 'Magnitude (dB)',
         };
         
@@ -63,8 +66,64 @@ module.exports = {
             ylabel: 'Phase (degrees)'
         };
 
-        return [new Dygraph(magnitude_div, magnitudes_data, _.extend(common_options, magnitude_options)),
-                new Dygraph(phase_div, phases_data, _.extend(common_options, phase_options))];
+        var graphs = [new Dygraph(magnitude_div, magnitudes_data, _.extend(options, magnitude_options)),
+                      new Dygraph(phase_div, phases_data, _.extend(options, phase_options))];
+        synchronize(graphs, {zoom: true, selection: true, range: false });
+        return graphs;
+    },
+
+    /**
+     * Plots a pole zero map of the given system.
+     * @param {HTMLElement} container - The container in which to put the plot.
+     * @param {System} sys - The system of which to plot the poles and zeros.
+     * @returns {Dygraph} A reference to the created plot
+     */
+    pzmap: function(container, sys) {
+        var zeros = sys.getZeros();
+        var poles = sys.getPoles();
+
+        var data = [];
+        for (var i = 0; i < zeros.length; ++i) {
+            var zero = zeros[i];
+            data.push([math.re(zero), math.im(zero), NaN]);
+        }
+        for (var j = 0; j < poles.length; ++j) {
+            var pole = poles[j];
+            data.push([math.re(pole), NaN, math.im(pole)]);
+        }
+
+        var options = {
+            legend: 'follow',
+            labelsDivStyles: {
+                'background-color':  'rgba(0, 0, 0, 0)'
+            },
+            labels: ['real', 'zeros', 'poles'],
+            drawPoints: true,
+            pointSize: 5,
+            xlabel: "Re",
+            ylabel: "Im",
+            strokeWidth: 0.0,
+            dateWindow: [-11, 11],
+            series: {
+                zeros: {
+                    drawPointCallback: shapes.CIRCLE,
+                    drawHighlightPointCallback: shapes.CIRCLE
+                },
+                poles: {
+                    drawPointCallback: shapes.EX,
+                    drawHighlightPointCallback: shapes.EX
+                }
+            },
+            axes: {
+                y: {
+                    valueRange: [-2, 2],
+                    valueFormatter: function(num) {
+                        return math.round(num, 5) + "j";
+                    }
+                }
+            }
+        };
+        return new Dygraph(container, data, options);
     },
 
     /**
