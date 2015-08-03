21c77077e6ece5d7c9f2ca77bc0c6a22bb1a42d8
mennovf <mennovanfrachem@hotmail.com>
2015-08-03 16:15:45 +0200

    Add tests for conversion and eval

--- javascript/test/system.js
+++ javascript/test/system.js
@@ -5,7 +5,7 @@ var system = require('../src/system');
 describe('System testing', function() {
 
     describe('zpk', function() {
-        it('correct construction', function() {
+        it('correct construction from zpk', function() {
             var z = [1, math.complex(1, 1)];
             var p = [8, math.complex(-5.7, 0)];
             var k = math.complex(8, -3.7);
@@ -16,10 +16,52 @@ describe('System testing', function() {
             assert.deepEqual(s.getPoles(), p);
             assert.deepEqual(s.getK(), k);
         });
+
+        it('correct construction from tf', function() {
+            var s = system.zpk(system.tf([1], [1, 0, 1]));
+
+            assert.deepEqual(s.getZeros(), []);
+            assert(math.equal(s.getPoles(), [math.complex(0, 1), math.complex(0, -1)]));
+            assert.equal(s.getK(), 1);
+        });
+    });
+
+    describe('tf', function() {
+        it('correct construction from num denom', function() {
+            var num = [1, 2, 3, 4];
+            var denom = [-1, -2, -3, -4, -5];
+            var s = system.tf(num, denom);
+
+            assert.deepEqual(s.getNumerator(), num);
+            assert.deepEqual(s.getDenominator(), denom);
+        });
+
+        it('correct construction from zpk', function() {
+            var s = system.tf(system.zpk([-2], [math.complex(0, 1), math.complex(0, -1)], 3));
+            
+            assert.deepEqual(s.getNumerator(), [3, 6]);
+            assert.deepEqual(s.getDenominator(), [1, math.complex(0, 0), math.complex(1, 0)]);
+        });
     });
 
 
-    describe('System#eval()', function() {
+    describe('ss', function() {
+        it('correct construction from ABCD', function() {
+            var A = [[1, -1], [2, 3]],
+                B = [0, 1],
+                C = [-3, 4],
+                D = [7];
+            var s = system.ss(A, B, C, D);
+
+            assert.deepEqual(s.A, A);
+            assert.deepEqual(s.B, B);
+            assert.deepEqual(s.C, C);
+            assert.deepEqual(s.D, D);
+        });
+    });
+
+
+    describe('Zpk#eval()', function() {
         it('works with regular numbers', function() {
             var s = system.zpk([1, -1, 5.3], [-4.2], 7);
 
@@ -35,6 +77,23 @@ describe('System testing', function() {
             assert.deepEqual(s.eval(2), math.complex('-8.5062240663900414938 - 5.6016597510373443983i'));
             assert.deepEqual(s.eval(math.complex('1.5 + 0.7i')),
                              math.complex('-2.2676342525399129173 - 2.0325108853410736i'));
+        });
+    });
+
+    describe('Tf#eval()', function() {
+        it('works with regular numbers', function() {
+            var s = system.tf([1, -1, 5.3], [-4.2]);
+
+            assert(math.equal(s.eval(2), -1.738095238));
+            assert(math.equal(s.eval(-3.7), -5.402380952));
+            assert(math.equal(s.eval(math.complex(0, 1)), math.complex(-1.023809524, 0.2380952381)));
+        });
+
+        it('works with complex numbers', function() {
+            var s = system.tf([math.complex(1, 1), math.complex(-3, 2.5)], [math.complex(1.2, -3)]);
+
+            assert(math.equal(s.eval(2), math.complex(-1.408045977, 0.2298850575)));
+            assert(math.equal(s.eval(math.complex(1.5, 0.7)), math.complex(-1.603448276, -0.09195402299)));
         });
     });
 });
