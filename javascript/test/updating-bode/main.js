c899daec76fb46062a48af830e456951332f44a6
mennovf <mennovanfrachem@hotmail.com>
2015-08-05 10:42:48 +0200

    Don't animage when updating bode

--- javascript/test/updating-bode/main.js
+++ javascript/test/updating-bode/main.js
@@ -13,7 +13,7 @@ zero_el.addEventListener('input', function() {
     var sys = system.zpk([zero], [10], 1);
     var bode_data = sys.bode(bounds);
 
-    plts[0].series[0].setData(bode_data.omegas.map(function(omega, i) {return [omega, bode_data.dBs[i]];}));
+    plts[0].series[0].setData(bode_data.omegas.map(function(omega, i) {return [omega, bode_data.dBs[i]];}), true, false, true);
 
-    plts[1].series[0].setData(bode_data.omegas.map(function(omega, i) {return [omega, bode_data.degrees[i]];}));
+    plts[1].series[0].setData(bode_data.omegas.map(function(omega, i) {return [omega, bode_data.degrees[i]];}), true, false, true);
 });
