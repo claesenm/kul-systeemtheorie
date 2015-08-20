1e5a39ff5cdc3db98dfb44b4960d8a65b27b8e96
StevenColleman <steven.colleman@student.kuleuven.be>
2015-08-20 12:33:41 +0200

    work am

--- javascript/demo's/demo3 PID controllers/mainPIDcontrollers.js
+++ javascript/demo's/demo3 PID controllers/mainPIDcontrollers.js
@@ -7,6 +7,10 @@ function main(){
 	document.getElementById('update').addEventListener('click', update);
 	
 	function update(){
+		var P = parseFloat(P_box.value || 1);
+		var I = parseFloat(I_box.value || 1);
+		var D = parseFloat(D_box.value || 1);
+			
 		var arrayNumerator = document.getElementById("numerator").value.toString().split(',');
 		while (arrayNumerator[0] == 0 && arrayNumerator.length > 1){
 			arrayNumerator.shift();
@@ -17,6 +21,24 @@ function main(){
 			arrayDenominator.shift();
 		}
 		var denominatorDegree = arrayDenominator.length - 1;
+		
+		var PIDnumerator = [D,P,I];
+		while (PIDnumerator[0] == 0 && PIDnumerator.length > 1){
+			PIDnumerator.shift();
+		}
+		var PIDnumeratordegree = PIDnumerator.length - 1;
+		
+		var closedLoopNumerator = control.num.conv(arrayNumerator,PIDnumerator);
+		while (closedLoopNumerator[0] == 0 && closedLoopNumerator.length > 1){
+			closedLoopNumerator.shift();
+		}
+		var closedLoopDenominator = control.num.polyadd(control.num.conv(arrayDenominator,[1,0]),control.num.conv(arrayNumerator,PIDnumerator));
+		while (closedLoopDenominator[0] == 0 && closedLoopDenominator.length > 1){
+			closedLoopDenominator.shift();
+		}
+		var closedLoopNumeratorDegree = closedLoopNumerator.length - 1;
+		var closedLoopDenominatorDegree = closedLoopDenominator.length - 1;
+		
 		var correct = 1;
 		//for (i = 0; i < arrayNumerator.length; i++){
 		//	if (!(arrayNumerator[i] typeof number)){
@@ -29,192 +51,181 @@ function main(){
 		//	}
 		//}
 		if (numeratorDegree > denominatorDegree){
-			alert("The degree of the numerator can not be bigger than the degree of the denominator.");
+			alert("The plant you developed is not stable!");
+		} else if (closedLoopNumeratorDegree > closedLoopDenominatorDegree){
+			alert("The closed loop system you developed is not stable!");
 		} else if (correct == 0){
 			alert("Please write your transfer function as in the example!");
 		} else if (denominatorDegree == 0 && arrayDenominator[0] == 0){
 			alert("Don't divide by zero please!");
 		} else if (numeratorDegree == 0 && arrayNumerator[0] == 0) {
-			alert("Don't take a transfer function of 0 please!");
-		}
-		else {
-		document.getElementById("Pvalue").innerHTML = parseFloat(P_box.value || 1);
-		document.getElementById("Ivalue").innerHTML = parseFloat(I_box.value || 1);
-		document.getElementById("Dvalue").innerHTML = parseFloat(D_box.value || 1);
-		var tempnumerator = "";
-		var tempdenominator = "";
-		if (numeratorDegree > 1){
-		for (i = 0; i < numeratorDegree - 1; i++){
-			if (i == 0){
-				if (arrayNumerator[i] == 1) {
-					tempnumerator = tempnumerator + "s<html><sup>b</sup></html>".replace("b", numeratorDegree - i);
-				}
-				else if (arrayNumerator[i] == -1) {
-					tempnumerator = tempnumerator + "-s<html><sup>b</sup></html>".replace("b", numeratorDegree - i);
-				}
-				else {
-					tempnumerator = tempnumerator + arrayNumerator[i].toString() + "s<html><sup>b</sup></html>".replace("b", numeratorDegree - i);
-				}
-			}
-			else {
-			if (arrayNumerator[i] > 0){
-				if (arrayNumerator[i] == 1) {
-					tempnumerator = tempnumerator + " " + "+" + " " + "s<html><sup>b</sup></html>".replace("b", numeratorDegree - i);
-				}
-				else {
-					tempnumerator = tempnumerator + " " + "+" + " " + arrayNumerator[i].toString() + "s<html><sup>b</sup></html>".replace("b", numeratorDegree - i);
-				}
-			}
-			else if (arrayNumerator[i] != 0){
-				if (arrayNumerator[i] == -1) {
-					tempnumerator = tempnumerator + " " + "-" + " " + "s<html><sup>b</sup></html>".replace("b", numeratorDegree - i);
-				}
-				else {
-					tempnumerator = tempnumerator + " " + "-" + " " + Math.abs(arrayNumerator[i]).toString() + "s<html><sup>b</sup></html>".replace("b", numeratorDegree - i);
-				}
-			}
-			}
-		}
-		if (arrayNumerator[numeratorDegree - 1] > 0) {
-			if (arrayNumerator[numeratorDegree - 1] == 1) {
-				tempnumerator = tempnumerator + " " + "+" + " " + "s";
-			}
-			else {
-				tempnumerator = tempnumerator + " " + "+" + " " + arrayNumerator[i].toString() + "s";
-			}
-		}
-		else if (arrayNumerator[numeratorDegree - 1] != 0){
-			if (arrayNumerator[numeratorDegree - 1] == -1) {
-				tempnumerator = tempnumerator + " " + "-" + " " + "s";
-			}
-			else {
-				tempnumerator = tempnumerator + " " + "-" + " " + Math.abs(arrayNumerator[i]).toString() + "s";
-			}
-		}
-		if (arrayNumerator[numeratorDegree] > 0){
-			tempnumerator = tempnumerator + " " + "+" + " " + arrayNumerator[numeratorDegree].toString();
-		}
-		else if (arrayNumerator[numeratorDegree] != 0)
-			tempnumerator = tempnumerator + " " + "-" + " " + Math.abs(arrayNumerator[numeratorDegree]).toString();
-		}
-		else {
-			if (numeratorDegree == 1){
-				if (arrayNumerator[numeratorDegree - 1] > 0) {
-					if (arrayNumerator[numeratorDegree - 1] == 1) {
-						tempnumerator = tempnumerator + "s";
-					}
-				else {
-					tempnumerator = tempnumerator + arrayNumerator[0].toString() + "s";
-					}
-				}
-				else {
-					if (arrayNumerator[numeratorDegree - 1] == -1) {
-						tempnumerator = tempnumerator + "-s";
-					}
-					else {
-						tempnumerator = tempnumerator + "-" + Math.abs(arrayNumerator[0]).toString() + "s";
-					}
-				}
-				if (arrayNumerator[numeratorDegree] > 0){
-					tempnumerator = tempnumerator + " " + "+" + " " + arrayNumerator[numeratorDegree].toString();
-				}
-				else if (arrayNumerator[numeratorDegree] != 0){
-					tempnumerator = tempnumerator + " " + "-" + " " + Math.abs(arrayNumerator[numeratorDegree]).toString();
-				}
-			}
-			else {
-				tempnumerator = tempnumerator + arrayNumerator[0].toString();
-			}
-		}
+			alert("Don't take a transfer function of zero please!");
+		} else if (P == 0 && I == 0 && D == 0){
+			alert("Don't take zero for every PID constant, please.");
+		} else if (numeratorDegree == denominatorDegree && D!= 0){
+			alert("If the degree of the numerator is equal to the degree of the denominator, the value of K_d has\n" + 
+			"to be equal to zero. Otherwise, the closed-loop system will become unstable.");
+		} else {
+			
+		document.getElementById("numerator_C").innerHTML = writeAsFormula(PIDnumerator,PIDnumeratordegree);
+		document.getElementById("denominator_C").innerHTML = writeAsFormula([1,0],1);
+		document.getElementById("numerator_plant").innerHTML = writeAsFormula(arrayNumerator,numeratorDegree);
+		document.getElementById("denominator_plant").innerHTML = writeAsFormula(arrayDenominator,denominatorDegree);
 		
-		if (denominatorDegree > 1){
-		for (i = 0; i < denominatorDegree - 1; i++){
-			if (i == 0){
-				if (arrayDenominator[i] == 1) {
-					tempdenominator = tempdenominator + "s<html><sup>b</sup></html>".replace("b", denominatorDegree - i);
-				}
-				else if (arrayDenominator[i] == -1) {
-					tempdenominator = tempdenominator + "-s<html><sup>b</sup></html>".replace("b", denominatorDegree - i);
-				}
-				else {
-					tempdenominator = tempdenominator + arrayDenominator[i].toString() + "s<html><sup>b</sup></html>".replace("b", denominatorDegree - i);
-				}
-			}
-			else {
-			if (arrayDenominator[i] > 0){
-				if (arrayDenominator[i] == 1) {
-					tempdenominator = tempdenominator + " " + "+" + " " + "s<html><sup>b</sup></html>".replace("b", denominatorDegree - i);
-				}
-				else {
-					tempdenominator = tempdenominator + " " + "+" + " " + arrayDenominator[i].toString() + "s<html><sup>b</sup></html>".replace("b", denominatorDegree - i);
-				}
-			}
-			else if (arrayDenominator[i] != 0){
-				if (arrayDenominator[i] == -1) {
-					tempdenominator = tempdenominator + " " + "-" + " " + "s<html><sup>b</sup></html>".replace("b", denominatorDegree - i);
-				}
-				else {
-					tempdenominator = tempdenominator + " " + "-" + " " + Math.abs(arrayDenominator[i]).toString() + "s<html><sup>b</sup></html>".replace("b", denominatorDegree - i);
-				}
-			}
-			}
+		
+		
+		document.getElementById("numerator_comp").innerHTML = writeAsFormula(closedLoopNumerator,closedLoopNumeratorDegree);
+		document.getElementById("denominator_comp").innerHTML = writeAsFormula(closedLoopDenominator,closedLoopDenominatorDegree);
+		
+		control.plot.pzmap(document.getElementById('polezeroplot'), control.system.tf(closedLoopNumerator,closedLoopDenominator));
+		
+		var plt2 = control.plot.step(document.getElementById('step-plot'), control.system.tf(closedLoopNumerator,closedLoopDenominator), [0, 20], true);
+		plt2.show_step_info({rise_time: true, settling_time: true, settled: true});
+		
+		//var info = control.num.stepinfo(plt2);
+		//document.getElementById("rise_time").innerHTML=round(info.rise_time);
+		//document.getElementById("final_value").innerHTML=round(info.final_value);
+		//document.getElementById("peak").innerHTML=round(info.peak);
+		//document.getElementById("peak_time").innerHTML=round(info.peak_time);
+		//document.getElementById("settling_time").innerHTML=round(info.settling_time);
 		}
-		if (arrayDenominator[denominatorDegree - 1] > 0) {
-			if (arrayDenominator[denominatorDegree - 1] == 1) {
-				tempdenominator = tempdenominator + " " + "+" + " " + "s";
-			}
-			else {
-				tempdenominator = tempdenominator + " " + "+" + " " + arrayDenominator[i].toString() + "s";
-			}
-		}
-		else if (arrayDenominator[denominatorDegree - 1] != 0){
-			if (arrayDenominator[denominatorDegree - 1] == -1) {
-				tempdenominator = tempdenominator + " " + "-" + " " + "s";
-			}
-			else {
-				tempdenominator = tempdenominator + " " + "-" + " " + Math.abs(arrayDenominator[i]).toString() + "s";
-			}
-		}
-		if (arrayDenominator[denominatorDegree] > 0){
-			tempdenominator = tempdenominator + " " + "+" + " " + arrayDenominator[denominatorDegree].toString();
-		}
-		else if (arrayDenominator[denominatorDegree] != 0)
-			tempdenominator = tempdenominator + " " + "-" + " " + Math.abs(arrayDenominator[denominatorDegree]).toString();
-		}
-		else {
-			if (denominatorDegree == 1){
-				if (arrayDenominator[denominatorDegree - 1] > 0) {
-					if (arrayDenominator[denominatorDegree - 1] == 1) {
-						tempdenominator = tempdenominator + "s";
-					}
-				else {
-					tempdenominator = tempdenominator + arrayDenominator[0].toString() + "s";
-					}
-				}
-				else {
-					if (arrayDenominator[denominatorDegree - 1] == -1) {
-						tempdenominator = tempdenominator + "-s";
-					}
-					else {
-						tempdenominator = tempdenominator + "-" + Math.abs(arrayDenominator[0]).toString() + "s";
-					}
-				}
-				if (arrayDenominator[denominatorDegree] > 0){
-					tempdenominator = tempdenominator + " " + "+" + " " + arrayDenominator[denominatorDegree].toString();
-				}
-				else if (arrayDenominator[denominatorDegree] != 0){
-					tempdenominator = tempdenominator + " " + "-" + " " + Math.abs(arrayDenominator[denominatorDegree]).toString();
-				}
-			}
-			else {
-				tempdenominator = tempdenominator + arrayDenominator[0].toString();
-			}
-		}
-		document.getElementById("numerator_plant").innerHTML = tempnumerator;
-		document.getElementById("denominator_plant").innerHTML = tempdenominator;
-		// here must come the changes of the diagrams.
 		}
 	}
-}
 
 window.onload = main;
+
+function writeAsFormula(array,arrayDegree){
+	var temp = "";
+	if (arrayDegree > 1){
+		for (i = 0; i < arrayDegree - 1; i++){
+			if (i == 0){
+				temp = writeFirstTermCaseDegreeGreaterThanOne(array,arrayDegree,temp,i);
+			}
+			else {
+				temp = writeTermNotFirstButDegreeAtLeastTwo(array,arrayDegree,temp,i);
+			}
+		}
+		if (array[arrayDegree - 1] > 0) {
+			temp = writeTermDegreeOneCasePositiveAndDegreeArrayAtLeastTwo(array,arrayDegree,temp,i);
+		}
+		else if (array[arrayDegree - 1] != 0){
+			temp = writeTermDegreeOneCaseNegativeAndDegreeArrayAtLeastTwo(array,arrayDegree,temp,i);
+		}
+		temp = writeConstantTermCaseDegreeAtLeastOne(array,arrayDegree,temp);
+	}
+		else {
+			if (arrayDegree == 1){
+				temp = writeDegreeOneCaseDegreeOne(array,arrayDegree,temp);
+				temp = writeConstantTermCaseDegreeAtLeastOne(array,arrayDegree,temp);
+			}
+			else {
+				temp = temp + array[0].toString();
+			}
+		}
+	return temp;
+}
+
+function writeFirstTermCaseDegreeGreaterThanOne(array,arrayDegree,temp,i){
+	var temp1 = temp;
+	if (array[i] == 1) {
+		temp1 = temp1 + "s<html><sup>b</sup></html>".replace("b", arrayDegree - i);
+	}
+	else if (array[i] == -1) {
+		temp1 = temp1 + "-s<html><sup>b</sup></html>".replace("b", arrayDegree - i);
+	}
+	else {
+		temp1 = temp1 + array[i].toString() + "s<html><sup>b</sup></html>".replace("b", arrayDegree - i);
+	}
+	return temp1;
+}
+
+function writeTermNotFirstButDegreeAtLeastTwoPositive(array,arrayDegree,temp,i){
+	var temp1 = temp;
+	if (array[i] == 1) {
+		temp1 = temp1 + " " + "+" + " " + "s<html><sup>b</sup></html>".replace("b", arrayDegree - i);
+	}
+	else {
+		temp1 = temp1 + " " + "+" + " " + array[i].toString() + "s<html><sup>b</sup></html>".replace("b", arrayDegree - i);
+	}
+	return temp1;
+}
+
+function writeTermNotFirstButDegreeAtLeastTwoNegative(array,arrayDegree,temp,i){
+	var temp1 = temp;
+	if (array[i] == -1) {
+		temp1 = temp1 + " " + "-" + " " + "s<html><sup>b</sup></html>".replace("b", arrayDegree - i);
+	}
+	else {
+		temp1 = temp1 + " " + "-" + " " + Math.abs(array[i]).toString() + "s<html><sup>b</sup></html>".replace("b", arrayDegree - i);
+	}
+	return temp1;
+}
+
+function writeTermNotFirstButDegreeAtLeastTwo(array,arrayDegree,temp,i){
+	var temp1 = temp;
+	if (array[i] > 0){
+		temp1 = writeTermNotFirstButDegreeAtLeastTwoPositive(array,arrayDegree,temp1,i);
+	}
+	else if (array[i] != 0){
+		temp1 = writeTermNotFirstButDegreeAtLeastTwoNegative(array,arrayDegree,temp1,i);
+	}
+	return temp1;
+}
+
+function writeTermDegreeOneCasePositiveAndDegreeArrayAtLeastTwo(array,arrayDegree,temp,i){
+	var temp1 = temp;
+	if (array[arrayDegree - 1] == 1) {
+		temp1 = temp1 + " " + "+" + " " + "s";
+	}
+	else {
+		temp1 = temp1 + " " + "+" + " " + array[i].toString() + "s";
+	}
+	return temp1;
+}
+
+function writeTermDegreeOneCaseNegativeAndDegreeArrayAtLeastTwo(array,arrayDegree,temp,i){
+	var temp1 = temp;
+	if (array[arrayDegree - 1] == -1) {
+		temp1 = temp1 + " " + "-" + " " + "s";
+	}
+	else {
+		temp1 = temp1 + " " + "-" + " " + Math.abs(array[i]).toString() + "s";
+	}
+	return temp1;
+}
+
+function writeConstantTermCaseDegreeAtLeastOne(array,arrayDegree,temp){
+	var temp1 = temp;
+	if (array[arrayDegree] > 0){
+		temp1 = temp1 + " " + "+" + " " + array[arrayDegree].toString();
+	}
+	else if (array[arrayDegree] != 0){
+		temp1 = temp1 + " " + "-" + " " + Math.abs(array[arrayDegree]).toString();
+	}
+	return temp1;
+}
+
+function writeDegreeOneCaseDegreeOne(array,arrayDegree,temp){
+	var temp1 = temp;
+	if (array[arrayDegree - 1] > 0) {
+		if (array[arrayDegree - 1] == 1) {
+			temp1 = temp1 + "s";
+		}
+		else {
+			temp1 = temp1 + array[0].toString() + "s";
+		}
+	}
+	else {
+		if (array[arrayDegree - 1] == -1) {
+			temp1 = temp1 + "-s";
+		}
+		else {
+			temp1 = temp1 + "-" + Math.abs(array[0]).toString() + "s";
+		}
+	}
+	return temp1;
+}
+
+function round(value){
+	return Math.round(10000*value)/10000;
+}
